753d05c247c3465c0014fdb38a1a4f4a10f6d6f2
Ya.nnis <ybarache@icloud.com>
2023-03-03 15:20:29 +0100

    Reglages java

--- src/TestBoxes.java
+++ src/TestBoxes.java
@@ -1,1 +1,9 @@
-package src;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+public class TestsBoxes{
+
+        @Test
+        public void testBoxCreate() {
+        Box b = new Box();
+        }
+};
