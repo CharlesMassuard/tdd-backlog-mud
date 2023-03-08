00f9529d856c8cbc768c6719b853ca569683b69a
MASSUARD Charles <o22201673@info27-15.iut45.univ-orleans.fr>
2023-03-08 14:46:44 +0100

    Ajout constructeur Box et méthode add + class Thing

--- src/Box.java
+++ src/Box.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+
+class Thing{
+    String name;
+    public Thing(String name){
+    this.name = name;
+    }
+}
+
 public class Box {
-    
+
+    ArrayList<String> contents = new ArrayList<String>();
+
+    public Box(){
+        System.out.println("Box créée");
+    }
+
+    public void add(String truc){
+        this.contents.add(truc);
+    }
 }
